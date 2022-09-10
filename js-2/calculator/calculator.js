9c0c707b7d2ca57d6c01add6b0d6c05e83aa4e25
MuWar47 <munailwaqar@gmail.com>
2022-09-10 17:33:29 +0500

    word wrap

--- js-2/calculator/calculator.js
+++ js-2/calculator/calculator.js
@@ -1,4 +1,3 @@
-const calculator = document.querySelector(".calculator");
 const display = document.querySelector(".calc_display");
 const all_btns = document.querySelectorAll("button");
 let displayValue = "";
@@ -6,7 +5,6 @@ let displayValue = "";
 all_btns.forEach(button => {
     button.addEventListener("click", (e) => {
         buttonText =  e.target.innerText;
-        console.log(buttonText);
         if (buttonText == "AC") {
             displayValue = "";
             display.innerHTML = displayValue;
